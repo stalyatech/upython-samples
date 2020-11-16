12c0b84a8dd9f1df512f55ac8d634e402c4244e8
Tayfun Karan <tayfunkaran@gmtcontrol.com>
2020-11-16 12:18:34 +0300

    Missing stub files have been added.

---
+++ .upython/stubs/ucryptolib.py
@@ -0,0 +1,14 @@
+"""
+Module: 'ucryptolib' on pySBC 1.13.0 with FW1.5.7-225
+"""
+# MCU: (sysname='pySBC', nodename='pySBC', release='1.13.0 with FW1.5.7', version='v1.13-225-g7d9d01bd1-dirty on 2020-11-15', machine='simpleRTK-SBC with STM32F745')
+# Stubber: 1.3.4
+
+class aes:
+    ''
+    def decrypt():
+        pass
+
+    def encrypt():
+        pass
+

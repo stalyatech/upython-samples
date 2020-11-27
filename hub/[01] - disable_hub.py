2e53efdc0e3c9888bebea7c5f3347635d61e3b32
volvox <volvox@stalyatech.com>
2020-11-27 12:52:10 +0300

    USB hub control samples have been added.

---
+++ hub/[01] - disable_hub.py
@@ -0,0 +1,12 @@
+import machine
+
+# ---------------------------------------------------------------
+# Power-On the USB Hub
+# ---------------------------------------------------------------
+pwr = machine.Power()
+pwr.off(machine.POWER_HUB)
+
+# Console info
+print('USB Hub has been disabled!')
+print('You don''t not see any serial port except MCU!')
+print('Ctrl+B helps to get command prompt;)')
---
+++ hub/[02] - enable_hub.py
@@ -0,0 +1,12 @@
+import machine
+
+# ---------------------------------------------------------------
+# Power-On the USB Hub
+# ---------------------------------------------------------------
+pwr = machine.Power()
+pwr.on(machine.POWER_HUB)
+
+# Console info
+print('USB Hub has been enabled!')
+print('You can see all serial ports now!')
+print('Ctrl+B helps to get command prompt;)')
